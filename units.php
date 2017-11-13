7699b29c8194a30d040aeae7041347c30d06f76c
Natalie Famula <nataliefamula@Randolphs-MacBook-Pro.local>
2017-11-13 18:14:45 -0500

    added miles to km conversion

--- units.php
+++ units.php
@@ -10,9 +10,18 @@ echo "weight: ";
 echo $pounds;
 echo " lb - ";
 echo $kilograms;
-echo "kg";
+echo " kg ";
 //number in miles want to convert to km
+$miles = 2.5;
 //floating pt value for the mile to km conversion
+$mile_to_km = 1.60934;
 //use the variables above to calculate miles multiplied by the kilometer conversion
+$kilometers = $miles * $mile_to_km;
 //display miles to kilometers
+echo "Distance: ";
+echo $miles;
+echo "miles = ";
+echo $kilometers;
+echo " km ";
+
 ?>
