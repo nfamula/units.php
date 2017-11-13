ab7c4a47acbd144d7453a0d0bd23282fd3b0d0bd
Natalie Famula <nataliefamula@Randolphs-MacBook-Pro.local>
2017-11-13 18:07:28 -0500

    pounds to kg conversion

--- units.php
+++ units.php
@@ -1,9 +1,16 @@
 <?php
 //number in pounds that we want to convert
-//floating pt value
+$pounds = 127;
+//floating pt value for kilo conversion
+$lbs_to_kg = 0.453592;
 //use variables above to calculate pounds multiplied by kilo conversion
+$kilograms = $pounds * $lbs_to_kg;
 //display pounds to kilograms
-
+echo "weight: ";
+echo $pounds;
+echo " lb - ";
+echo $kilograms;
+echo "kg";
 //number in miles want to convert to km
 //floating pt value for the mile to km conversion
 //use the variables above to calculate miles multiplied by the kilometer conversion
