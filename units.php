952f12fe0175e0d19e26db806fe50d083d182180
Natalie Famula <nataliefamula@Randolphs-MacBook-Pro.local>
2017-11-13 18:02:16 -0500

    added comments of plan for exercise

---
+++ units.php
@@ -0,0 +1,11 @@
+<?php
+//number in pounds that we want to convert
+//floating pt value
+//use variables above to calculate pounds multiplied by kilo conversion
+//display pounds to kilograms
+
+//number in miles want to convert to km
+//floating pt value for the mile to km conversion
+//use the variables above to calculate miles multiplied by the kilometer conversion
+//display miles to kilometers
+?>
